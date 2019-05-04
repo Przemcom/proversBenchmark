1150635811a3014506e6094446408dcc30cff7e1
Mateusz Grzelinski <czubaka111@gmail.com>
2019-05-04 20:01:36 +0200

    Use argparse
    Remove unnecessary parts

--- main.py
+++ main.py
@@ -1,77 +1,22 @@
-import os
-import toml as toml
-# import nltk
-import subprocess
+import argparse
 
-# current working directory
-cwd = os.path.abspath(os.path.dirname(__file__))
-
-default_config_fname = os.path.join(cwd, 'default_config.toml')
-config_fname = os.path.join(cwd, 'config.toml')
+from src.config import Config
 
 
-def my_setup():
-    """
-    Loads default config file with fields description and asks user for
-    parameters.
-    """
-    new_params = {}
-    params = load_params(fname=config_fname)
-    sep = 8 * " " + 8 * "*" + 8 * " "
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Provers Benchmark",
+                                     description="Benchmark for testing provers")
+    parser.add_argument("-v", "--version", action="version",
+                        version="%(prog)s Pre-alpha 0.1",
+                        help="Prints current version")
+    parser.add_argument("-f", "--file", default="config.toml", help="config file")
 
-    print(sep + "You will now have to set parameters for benchmark.\n" +
-          sep + "    Default values are printed between brackets.\n" +
-          sep + "    Leave blank if you want to use default value")
-
-    for param in params:
-        new_params[param] = {}
-        values = params[param]['value']
-        descriptions = params[param]['description']
-        for value in values:
-            new_params[param][value] = ""
-            if not values[value]:
-                input_msg = f"{descriptions[value]} : "
-            else:
-                input_msg = f"{descriptions[value]} [{values[value]}] : "
-            while not new_params[param][value]:
-                input_var = input(input_msg)
-                if not input_var:
-                    new_params[param][value] = values[value]
-                else:
-                    new_params[param][value] = input_var
-    print(new_params)
-    with open(config_fname, 'w', encoding='utf-8') as fp:
-        toml.dump(new_params, fp)
-    return new_params
-
-
-def load_params(fname=config_fname, param=""):
-    """
-    Load parameters from file.
-
-    If config file doesn't exist, we ask user to build one
-    """
-    params = {}
-    if not os.path.isfile(fname):
-        print()
-        params = my_setup()
-
-    if not params:
-        with open(fname, 'r', encoding='utf-8') as fp:
-            params.update(toml.load(fp))
-
-    if param:
-        return params[param]
-    else:
-        return params
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    # jeszcze nie uwzgledniam w mainie load_params() ani my_setup(), nawet nie wiem czy sa dobrze napisane
-    tptp_file = os.path.join(cwd, 'tests/data/test.p')
-    # command = f"tptp_to_ladr < {tptp_file} | prover9 > {cwd}/tests/ladr_file.out"
-    # print(command.split(), end='')
-    # print(subprocess.check_call(command.split()))
-    # subprocess.Popen(command.split())
-    # subprocess.call(command.split()) #Popen ani call nie dzialaja, czemu?
-    # os.system(command)
+
+    args = parse_args()
+
+    config = Config(config_file=args.file)
+    config.load_config()
