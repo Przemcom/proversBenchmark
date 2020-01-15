f0393656401047292391b970d3064735bcfa0a9b
Mateusz Grzelinski <czubaka111@gmail.com>
2020-01-15 20:41:06 +0100

    Dynamically load statistics

--- src/parsers/statistics_parsers/inkresat_cnf_json_parser.py
+++ src/parsers/statistics_parsers/inkresat_cnf_json_parser.py
@@ -14,12 +14,10 @@ class InkresatCNFPTLStatisticParser(StatisticParser):
         assert file_path.endswith('.json'), 'inkresat parser expects json files for statistics'
         with open(file_path, 'r') as fp:
             dict = json.load(fp)
-        return ConjunctiveNormalFormPropositionalTemporalLogicFormulaInfo(
-            number_of_variables=dict['number_of_variables'],
-            number_of_clauses=dict['number_of_clauses'],
-            clause_sizes={int(key): int(val) for key, val in dict['clause_sizes'].items()},
-            number_of_variables_with_always_connectives=dict['number_of_variables_with_always_connectives'],
-            number_of_variables_with_eventually_connectives=dict['number_of_variables_with_eventually_connectives'],
-            number_of_variables_without_connective=dict['number_of_variables_without_connective'],
-            max_clause_size=dict['max_clause_size']
-        )
+        stats = ConjunctiveNormalFormPropositionalTemporalLogicFormulaInfo()
+        for key, val in dict.items():
+            if key == 'clause_sizes':
+                setattr(stats, key, {int(key): int(val) for key, val in dict['clause_sizes'].items()})
+            else:
+                setattr(stats, key, val)
+        return stats
--- src/statistics/stats.py
+++ src/statistics/stats.py
@@ -102,6 +102,7 @@ class ConjunctiveNormalFormPropositionalTemporalLogicFormulaInfo:
     number_of_variables_with_always_connectives: int = 0
     number_of_variables_with_eventually_connectives: int = 0
     number_of_variables_without_connective: int = 0
+    number_of_negated_variables: int = 0
 
 
 class SATStatus(Enum):
